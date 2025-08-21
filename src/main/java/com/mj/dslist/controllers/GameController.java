3bcc2294d201777de29da1d2a5cf539e9fecb6a7
Marcio Gomes <mjgomes1978@gmail.com>
2025-08-20 21:32:07 -0300

    GET game by id.

--- src/main/java/com/mj/dslist/controllers/GameController.java
+++ src/main/java/com/mj/dslist/controllers/GameController.java
@@ -1,10 +1,11 @@
 package com.mj.dslist.controllers;
 
+import com.mj.dslist.dto.GameDTO;
 import com.mj.dslist.dto.GameMinDTO;
-import com.mj.dslist.entities.Game;
 import com.mj.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class GameController {
      public List<GameMinDTO> findAll() {
         // Fetch all games from the service
         List<GameMinDTO> result = gameService.findAll();
+        return result;
+     }
+
+     @GetMapping(value = "/{id}")
+     public GameDTO findById(@PathVariable Long id) {
+        // Fetch a game by ID from the service
+        GameDTO result = gameService.findById(id);
         return result;
      }
 }
--- src/main/java/com/mj/dslist/services/GameService.java
+++ src/main/java/com/mj/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.mj.dslist.services;
 
+import com.mj.dslist.dto.GameDTO;
 import com.mj.dslist.dto.GameMinDTO;
 import com.mj.dslist.entities.Game;
 import com.mj.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +17,14 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        // Fetch the game by ID from the repository and convert it to GameDTO
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+
+    }
 
     public List<GameMinDTO> findAll() {
         // Fetch all games from the repository and convert them to GameMinDTO
